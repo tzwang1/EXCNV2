56f5dee8a4b9583b6a015c9b2edb7809daa9184f
tzwang1 <truman.wang1234@gmail.com>
2018-03-28 00:00:05 +0000

    changing num training examples

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -71,7 +71,7 @@ if torch.cuda.is_available():
 ###############################################################################
 
 # corpus = data.Corpus(args.data)
-num = 7000000
+num = 1000000
 #num = 100000
 seq_len = 30
 corpus = data.Corpus(args.input_train, args.target_train, args.input_val, args.target_val, args.input_test, args.target_test, num, seq_len)
