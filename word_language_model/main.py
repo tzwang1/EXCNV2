3556dcc4f99d2ad6fe3deab510af88f4cd71be28
Truman Wang <truman.wang@mail.utoronto.ca>
2018-03-29 08:33:54 -0400

    printing out target values

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -74,7 +74,7 @@ if torch.cuda.is_available():
 # corpus = data.Corpus(args.data)
 # num = 100000
 # seq_len = 30
-num = 100000
+num = 1000000
 seq_len = 100
 
 corpus = data.Corpus(args.input_train, args.target_train, args.input_val, args.target_val, args.input_test, args.target_test, num, seq_len)
@@ -140,7 +140,8 @@ if args.cuda:
     model.cuda()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr = 0.01, momentum=0.9)
+# optimizer = optim.SGD(model.parameters(), lr = 0.01, momentum=0.9)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)
 
 ###############################################################################
 # Training code
