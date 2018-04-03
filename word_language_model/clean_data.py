29acd03595453e02df38c9d64eeb35ce12422131
Truman Wang <truman.wang@mail.utoronto.ca>
2018-04-03 18:08:20 -0400

    adding script to clean data

--- word_language_model/clean_data.py
+++ word_language_model/clean_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 input_path = 
 output_path = 
 
-x = pd.read_csv(input_path, sep='\t')
+x = pd.read_csv(input_path, sep='\t', header=None)
 x[x.columns[0]] = x[x.columns[0]].map(lambda x: x.lstrip('chr'))
 x[x.columns[2]] = x = x[x.columns[2]].str.upper()
 
