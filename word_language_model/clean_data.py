4141159463776b093d04c9e58f8d93a75291ec86
Truman Wang <truman.wang@mail.utoronto.ca>
2018-04-03 18:25:44 -0400

    updating script to work with three inputs

--- word_language_model/clean_data.py
+++ word_language_model/clean_data.py
@@ -1,10 +1,24 @@
 import pandas as pd
 
-input_path = "data/good_data/input_val.out" 
-output_path = "data/good_data/input_val2.out"
+def clean(input_path, output_path):
+    x = pd.read_csv(input_path, sep='\t', header=None)
+    x[x.columns[0]] = x[x.columns[0]].map(lambda x: x.lstrip('chr'))
+    x[x.columns[2]] = x[x.columns[2]].str.upper()
 
-x = pd.read_csv(input_path, sep='\t', header=None)
-x[x.columns[0]] = x[x.columns[0]].map(lambda x: x.lstrip('chr'))
-x[x.columns[2]] = x[x.columns[2]].str.upper()
+    x.to_csv(output_path, sep='\t', index=False)
 
-x.to_csv(output_path, sep='\t', index=False)
+
+input_train = "data/good_data/input_train.out" 
+output_train = "data/good_data/input_train2.out"
+
+input_val = "data/good_data/input_val.out" 
+output_val = "data/good_data/input_val2.out"
+
+input_test = "data/good_data/input_test.out" 
+output_test = "data/good_data/input_test2.out"
+
+clean(input_train, output_train)
+clean(input_val, output_val)
+clean(input_test, output_test)
+
+
