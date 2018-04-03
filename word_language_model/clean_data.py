910d1d6ec7afa9901c8e5962e0b6a7f0abbcbe38
Truman Wang <truman.wang@mail.utoronto.ca>
2018-04-03 18:22:15 -0400

    fixing bug

--- word_language_model/clean_data.py
+++ word_language_model/clean_data.py
@@ -5,6 +5,6 @@ output_path =
 
 x = pd.read_csv(input_path, sep='\t', header=None)
 x[x.columns[0]] = x[x.columns[0]].map(lambda x: x.lstrip('chr'))
-x[x.columns[2]] = x = x[x.columns[2]].str.upper()
+x[x.columns[2]] = x[x.columns[2]].str.upper()
 
 x.to_csv(output_path, sep='\t', index=False)
