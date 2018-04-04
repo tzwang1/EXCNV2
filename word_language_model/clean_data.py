78a4d54b33628e70ccedea9b351b230432cf1b08
Truman Wang <truman.wang@mail.utoronto.ca>
2018-04-03 21:11:48 -0400

    fixing

--- word_language_model/clean_data.py
+++ word_language_model/clean_data.py
@@ -4,7 +4,7 @@ def clean(input_path, output_path):
     x = pd.read_csv(input_path, sep='\t', header=None)
     x[x.columns[0]] = x[x.columns[0]].map(lambda x: x.lstrip('chr'))
     x[x.columns[2]] = x[x.columns[2]].str.upper()
-    x[x.columns[3]] = x[x.columns[3]].astype(int)
+    # x[x.columns[3]] = x[x.columns[3]].astype(int)
 
     x.to_csv(output_path, sep='\t', index=False)
 
