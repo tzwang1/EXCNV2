6f220a77c5453ed0015d55196942ec60c646a6fb
Truman Wang <truman.wang@mail.utoronto.ca>
2018-04-04 20:55:22 -0400

    added argparse

--- word_language_model/clean_data.py
+++ word_language_model/clean_data.py
@@ -1,25 +1,23 @@
 import pandas as pd
+import argparse
 
-def clean(input_path, output_path):
+parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM Language Model')
+parser.add_argument('--input', type=str, default='data/input.out',
+                    help='location of the input file data')
+
+args = parser.parse_args()
+
+def clean(input_path):
     x = pd.read_csv(input_path, sep='\t', header=None)
     x[x.columns[0]] = x[x.columns[0]].map(lambda x: x.lstrip('chr'))
     x[x.columns[2]] = x[x.columns[2]].str.upper()
     # x[x.columns[3]] = x[x.columns[3]].astype(int)
 
-    x.to_csv(output_path, sep='\t', index=False)
+    x.to_csv(input_path, sep='\t', index=False, header=False)
 
 
-input_train = "data/good_data/input_train.out" 
-output_train = "data/good_data/input_train2.out"
-
-input_val = "data/good_data/input_val.out" 
-output_val = "data/good_data/input_val2.out"
-
-input_test = "data/good_data/input_test.out" 
-output_test = "data/good_data/input_test2.out"
-
-clean(input_train, output_train)
-clean(input_val, output_val)
-clean(input_test, output_test)
+clean(args.input)
+# clean(input_val, output_val)
+# clean(input_test, output_test)
 
 
