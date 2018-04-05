d2bb117a0781d9676623b6574c8b3570f57b966d
Truman Wang <truman.wang@mail.utoronto.ca>
2018-04-05 14:38:06 -0400

    automating data preprocessing

--- word_language_model/clean_data.py
+++ word_language_model/clean_data.py
@@ -3,12 +3,11 @@ import argparse
 import data
 
 parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM Language Model')
-parser.add_argument('--input', type=str, default='data/input.out',
-                    help='location of the input file data')
-parser.add_argument('--target', type=str, default='data/target.out',
+parser.add_argument('--data_list', type=str, default='data/input.out',
                     help='location of the input file data')
 
 args = parser.parse_args()
+data = pd.read_csv(args.data_list, sep=",", header=None)
 
 def clean(input_path):
     x = pd.read_csv(input_path, sep='\t', header=None)
@@ -18,8 +17,19 @@ def clean(input_path):
 
     x.to_csv(input_path, sep='\t', index=False, header=False)
 
+for i in range(len(data)):
+    clean(data[i][0])
 
-clean(args.input)
+all_data_x = []
+all_data_y = []
+
+for i in range(len(data)):
+    data_x, data_y = data.load_data_from_file(data[i][0], data[i][1])
+    all_data_x += data_x
+    all_data_y += data_y
+
+data.save(all_data_x, all_data_y, 'data_x.pl', 'data_y.pl')
+
 # clean(input_val, output_val)
 # clean(input_test, output_test)
 
