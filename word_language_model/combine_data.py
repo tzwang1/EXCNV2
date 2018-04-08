e8246c8f89270d4ef7932e20143e539abdff9001
truman wang <truman.wang1234@gmail.com>
2018-04-08 12:06:28 -0400

    fixing order of input and target combination

--- word_language_model/combine_data.py
+++ word_language_model/combine_data.py
@@ -6,7 +6,7 @@ import argparse
 import data
 
 parser = argparse.ArgumentParser(description='Combine all input and target pickle files')
-parser.add_argument('--data', type=str, default='',
+parser.add_argument('--data', type=str, default='data_to_transfer/data',
                     help='folder containing input and target pickle files that need to be combined')
 
 args = parser.parse_args()
@@ -20,15 +20,16 @@ target_path = os.path.join(args.data, "*_tar.pl")
 input_data_list = glob.glob(input_path)
 target_data_list = glob.glob(target_path)
 
-for file in glob.glob(input_path):
-    print(file)
-    with open(file) as f:
+
+for infile in sorted(input_data_list):
+    print(infile)
+    with open(infile) as f:
         data_x = pickle.load(f)
     all_data_x += data_x
 
-for file in glob.glob(target_path):
-    print(file)
-    with open(file) as f:
+for tarfile in sorted(target_data_list):
+    print(tarfile)
+    with open(tarfile) as f:
         data_y = pickle.load(f)
     all_data_y += data_y
 
