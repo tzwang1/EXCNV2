9748eca66a8350ed3424e04bdd28e48672a664e4
Truman Wang <truman.wang@mail.utoronto.ca>
2018-04-09 17:33:35 -0400

    removing

--- EXCNV2/clean_and_toupper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dir=data
-
-for filename in "$dir"/*.out; do
-  submitjob -m 10  sed -i 's/chr//' "$filename"
-done
--- EXCNV2/prepare_input_output.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dir=data
-
-while IFS=, read input_path target_path;do
-    python ../../clean_data_single.py --input_data $input_path --target_data $target_path --output_dir clean
-done < data_list.txt
-
